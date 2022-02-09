f05d8717e2a8be9849dd57a4a6b6f8d306d8ef35
nikitha <nikitha@LAPTOP-4GAKUODQ>
2022-02-09 16:08:47 +0530

    java literal program is added to github

---
+++ 03-02-2022/src/com/javaLiterals/Example1.java
@@ -0,0 +1,53 @@
+package com.javaLiterals;
+
+public class Example1 {
+
+	public static void main(String[] args) 
+	{
+		/**
+		 * write a java program to print learner details(id,name,age,marks,address,result,grade)
+		 */
+		  int learnerId = 1066;
+		  String learnerName= "Nikitha";
+		  int learnerAge = 21;
+		  float learnerMarks = 9.33f;
+		  String learnerAddress = "Hyderabad";
+		  boolean learnerResult = true;
+		  char learnerGrade = 'A';
+		  
+		  System.out.print("Learner Id :");
+		  System.out.println(learnerId);
+		  
+		  System.out.print("Learner Name :");
+		  System.out.println(learnerName);
+		  
+		  System.out.print("Learner Age :");
+		  System.out.println(learnerAge);
+		  
+		  
+		  System.out.print("Learner Marks :");
+		  System.out.println(learnerMarks);
+		  
+		  System.out.print("Learner Address :");
+		  System.out.println(learnerAddress);
+		  
+		  System.out.print("Learner Result :");
+		  System.out.println(learnerResult);
+		  
+		  System.out.print("Learner Grade :");
+		  System.out.println(learnerGrade);
+		  
+	
+		  
+	
+		  
+	
+		  
+	
+		  
+	
+		
+
+	}
+
+}
